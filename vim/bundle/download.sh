d2f54c4cd0889c09fc799dc896dba055b15ea6ec
Kai Wells <support@kaiwells.me>
2017-12-16 11:06:49 -0800

    Vim plugins and indentation defaults

---
+++ vim/bundle/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Base packages
+git clone https://github.com/ajh17/Spacegray.vim;
+git clone https://github.com/tpope/vim-commentary;
+git clone https://github.com/tpope/vim-fugitive;
+
+# Language support
+git clone https://github.com/pangloss/vim-javascript;
+git clone https://github.com/fatih/vim-go.git;
+git clone https://github.com/dgryski/vim-godef;
+
---
+++ vim/ftplugin/python.vim
@@ -0,0 +1,5 @@
+set tabstop=4
+set softtabstop=0
+set shiftwidth=4
+set expandtab
+set smarttab
